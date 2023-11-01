ced189e19a9cb9eafe3e064855b407438f92b027
BiliRZ <2779264714@qq.com>
2023-11-01 21:40:19 +0800

    docs(package)

--- src/store/urlStore.ts
+++ src/store/urlStore.ts
@@ -4,9 +4,9 @@ export const useUrlStore = defineStore({
   id: 'urlStore',
 
   state: () => ({
-    apiUrl: 'http://localhost:5000',
+    // apiUrl: 'http://localhost:5000',
     // cosUrl: 'https://cos-test.bilirz.com',
-    // apiUrl: `${window.location.protocol}//${window.location.host}`,
+    apiUrl: `${window.location.protocol}//${window.location.host}`,
     cosUrl: 'https://cos.bilirz.com',
   })
 });
